a6e5e689e96de524fcd480a412dd37e6b85454a3
harshrajatul <127780941+harshrajatul@users.noreply.github.com>
2023-09-19 02:32:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
 
+let start_time = document.getElementById("start_time");
+let end_time = document.getElementById("end_time");
+
 song.onloadedmetadata = function(){
     progress.max = song.duration;
     progress.value = song.currentTime
@@ -34,3 +37,33 @@ progress.onchange = function(){
     ctrlIcon.classList.remove("fa-play");
     ctrlIcon.classList.add("fa-pause");
 }
+
+function PlaytoPause()
+{
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+}
+
+song.addEventListener("timeupdate", (event) => {
+    const {currentTime, duration} = event.srcElement;
+    
+    let min_dur = Math.floor(duration/60);
+    let sec_dur = Math.floor(duration%60);
+
+    let tot_dur = `${min_dur}:${sec_dur}`;
+    if(duration)
+    {
+        end_time.textContent = tot_dur;
+    }
+    // 
+    // 
+    let min_curr_dur = Math.floor(currentTime/60);
+    let sec_curr_dur = Math.floor(currentTime%60);
+
+    if(sec_curr_dur < 10)
+    {
+        sec_curr_dur = `0${sec_curr_dur}`
+    }
+    let tot_curr_dur = `${min_curr_dur}:${sec_curr_dur}`;
+    start_time.textContent = tot_curr_dur;
+})
